39ef8b065cff0ec4410b0b3c4432a91fe985dee4
末城via <mochengvia@panuon.com>
2024-09-16 10:30:25 +0800

    init

---
+++ src/shared/Panuon.WPF.Charts/Compositions/Segments/RadarSeriesSegment.cs
@@ -0,0 +1,79 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Panuon.WPF.Charts
+{
+    public class RadarSeriesSegment
+        : SegmentBase
+    {
+        #region Properties
+
+        #region LabelForeground
+        public Brush LabelForeground
+        {
+            get { return (Brush)GetValue(LabelForegroundProperty); }
+            set { SetValue(LabelForegroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelForegroundProperty =
+            DependencyProperty.Register("LabelForeground", typeof(Brush), typeof(RadarSeriesSegment), new PropertyMetadata(null, OnRenderPropertyChanged));
+        #endregion
+
+        #region LabelStroke
+        public Brush LabelStroke
+        {
+            get { return (Brush)GetValue(LabelStrokeProperty); }
+            set { SetValue(LabelStrokeProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelStrokeProperty =
+            DependencyProperty.Register("LabelStroke", typeof(Brush), typeof(RadarSeriesSegment), new PropertyMetadata(null, OnRenderPropertyChanged));
+        #endregion
+
+        #region LabelStrokeThickness
+        public double LabelStrokeThickness
+        {
+            get { return (double)GetValue(LabelStrokeThicknessProperty); }
+            set { SetValue(LabelStrokeThicknessProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelStrokeThicknessProperty =
+            DependencyProperty.Register("LabelStrokeThickness", typeof(double), typeof(RadarSeriesSegment), new PropertyMetadata(0.3, OnRenderPropertyChanged));
+        #endregion
+
+        #region Fill
+        public Brush Fill
+        {
+            get { return (Brush)GetValue(FillProperty); }
+            set { SetValue(FillProperty, value); }
+        }
+
+        public static readonly DependencyProperty FillProperty =
+            DependencyProperty.Register("Fill", typeof(Brush), typeof(RadarSeriesSegment), new PropertyMetadata(Brushes.Black, OnRenderPropertyChanged));
+        #endregion
+
+        #region Stroke
+        public Brush Stroke
+        {
+            get { return (Brush)GetValue(StrokeProperty); }
+            set { SetValue(StrokeProperty, value); }
+        }
+
+        public static readonly DependencyProperty StrokeProperty =
+            DependencyProperty.Register("Stroke", typeof(Brush), typeof(RadarSeriesSegment), new PropertyMetadata(null, OnRenderPropertyChanged));
+        #endregion
+
+        #region StrokeThickness
+        public double StrokeThickness
+        {
+            get { return (double)GetValue(StrokeThicknessProperty); }
+            set { SetValue(StrokeThicknessProperty, value); }
+        }
+
+        public static readonly DependencyProperty StrokeThicknessProperty =
+            DependencyProperty.Register("StrokeThickness", typeof(double), typeof(RadarSeriesSegment), new PropertyMetadata(1d, OnRenderPropertyChanged));
+        #endregion
+
+        #endregion
+    }
+}
---
+++ src/shared/Panuon.WPF.Charts/Compositions/Series/RadarSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Panuon.WPF.Charts
+{
+    public class RadarSeries
+        : ValueProviderSegmentsSeriesBase<PieSeriesSegment>
+    {
+        #region Methods
+
+        #region OnHighlighting
+        protected override void OnHighlighting(IDrawingContext drawingContext, IChartContext chartContext, ILayerContext layerContext, in IList<SeriesTooltip> tooltips)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region OnRendering
+        protected override void OnRendering(IDrawingContext drawingContext, IChartContext chartContext, double animationProgress)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #endregion
+    }
+}
